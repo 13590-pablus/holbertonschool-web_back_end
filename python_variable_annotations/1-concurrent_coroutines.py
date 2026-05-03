2b195a488d7ced988a9ccdd7a22dde0f3d19d9f5
13590-pablus <13590@holbertonstudents.com>
2026-05-03 09:21:28 +0000

    Complete all async tasks (0-4)

--- python_variable_annotations/1-concurrent_coroutines.py
+++ python_variable_annotations/1-concurrent_coroutines.py
@@ -7,6 +7,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Spawns wait_random n times with the specified max_delay """
+    """ Spawns wait_random n times and returns sorted delays """
     delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     return sorted(delays)
--- python_variable_annotations/2-measure_runtime.py
+++ python_variable_annotations/2-measure_runtime.py
@@ -7,8 +7,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """ Measures the total execution time for wait_n """
-    start_time = time.perf_content_base_time() if hasattr(time, 'perf_content_base_time') else time.time()
+    """ Returns total_time / n """
+    start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.perf_content_base_time() if hasattr(time, 'perf_content_base_time') else time.time()
+    end_time = time.time()
     return (end_time - start_time) / n
