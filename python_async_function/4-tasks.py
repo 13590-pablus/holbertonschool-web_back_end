cfea7d08de54a6597f9be5837d75fbebc954dc3e
13590-pablus <13590@holbertonstudents.com>
2026-05-03 09:38:15 +0000

    Task 4: Update task_wait_n with correct logic

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-"""Using tasks in wait_n."""
+"""
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is being called.
+"""
 import asyncio
 from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn task_wait_random n times."""
-    delays = []
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn task_wait_random n times and return the list of all the delays.
+    """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-
+    delays = [await task for task in asyncio.as_completed(tasks)]
     return delays
